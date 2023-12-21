50a61312e7c38cce28c2bcf041de0b8cb517fa45
liting <dosa1127@gmail.com>
2023-12-21 17:34:03 +0800

    [Feature] Generate story based on random picture in NotificationsFragment.

--- app/src/main/java/com/dosa/genai/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/dosa/genai/ui/notifications/NotificationsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -34,6 +35,13 @@ class NotificationsFragment : Fragment() {
         notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        val imageView: ImageView = binding.image
+        notificationsViewModel.image.observe(viewLifecycleOwner) {
+            imageView.setImageBitmap(it)
+        }
+
+        notificationsViewModel.generateStory()
         return root
     }
 
--- app/src/main/java/com/dosa/genai/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/dosa/genai/ui/notifications/NotificationsViewModel.kt
@@ -1,18 +1,56 @@
 package com.dosa.genai.ui.notifications
 
+import android.graphics.Bitmap
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.bumptech.glide.RequestManager
+import com.dosa.genai.ai.GeminiManager
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class NotificationsViewModel
 @Inject constructor(private val glide: RequestManager) : ViewModel() {
 
+    private val geminiManager: GeminiManager = GeminiManager()
+
+    private val _image = MutableLiveData<Bitmap>()
+    val image: LiveData<Bitmap> = _image
+
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
     }
     val text: LiveData<String> = _text
+
+    fun generateStory() {
+        val imageUrl = "https://picsum.photos/360?random=" + System.currentTimeMillis()
+
+        viewModelScope.launch(Dispatchers.IO) {
+            var bitmap: Bitmap? = null
+            try {
+                bitmap = getBitmap(imageUrl)
+            } catch (e: Exception) {
+                Timber.e(e)
+                _text.postValue(e.message)
+            }
+            bitmap?.let {
+                _image.postValue(bitmap!!)
+                val story = geminiManager.gen(bitmap)
+                _text.postValue(story)
+            }
+        }
+    }
+
+    private fun getBitmap(imageUrl: String): Bitmap {
+        return glide
+            .asBitmap()
+            .load(imageUrl)
+            .submit()
+            .get()
+    }
 }
