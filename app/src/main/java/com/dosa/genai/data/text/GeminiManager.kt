62835d579afe32d820c5651965684e07d5cd1ba5
liting <dosa1127@gmail.com>
2023-12-22 18:24:38 +0800

    [Modify] Remove test code in GeminiManager.

--- app/src/main/java/com/dosa/genai/data/text/GeminiManager.kt
+++ app/src/main/java/com/dosa/genai/data/text/GeminiManager.kt
@@ -16,9 +16,9 @@ class GeminiManager {
         private val config by lazy {
             generationConfig {
                 temperature = 0.9f
-                topK = 20
                 topP = 0.75f
-                maxOutputTokens = 300
+                topK = 32
+                maxOutputTokens = 1000
             }
         }
 
@@ -46,9 +46,11 @@ class GeminiManager {
                 safetySettings = safetySettings
             )
         }
+
+        private const val MAX_TOKENS = " << To Be Continued... >>"
     }
 
-    suspend fun gen(prompt: String = "Write a short story about a magic backpack."): String {
+    suspend fun gen(prompt: String): String {
         val sb = StringBuilder()
         try {
             textModel.generateContentStream(prompt).collect { chunk ->
@@ -56,15 +58,12 @@ class GeminiManager {
                 sb.append(chunk.text)
             }
         } catch (e: ResponseStoppedException) {
-            sb.append(" << MAX_TOKENS >>")
+            sb.append(MAX_TOKENS)
         }
         return sb.toString()
     }
 
-    suspend fun gen(
-        image: Bitmap,
-        text: String = "Write a short story inspired by this picture"
-    ): String {
+    suspend fun gen(image: Bitmap, text: String): String {
         val inputContent = content {
             image(image)
             text(text)
@@ -77,51 +76,7 @@ class GeminiManager {
                 sb.append(chunk.text)
             }
         } catch (e: ResponseStoppedException) {
-            sb.append(" << MAX_TOKENS >>")
-        }
-        return sb.toString()
-    }
-
-    suspend fun gen(
-        image1: Bitmap,
-        image2: Bitmap,
-        text: String = "What's different between these pictures?"
-    ): String {
-        val inputContent = content {
-            image(image1)
-            image(image2)
-            text(text)
-        }
-
-        val sb = StringBuilder()
-        try {
-            textImageModel.generateContentStream(inputContent).collect { chunk ->
-                Timber.d(chunk.text)
-                sb.append(chunk.text)
-            }
-        } catch (e: ResponseStoppedException) {
-            sb.append(" << MAX_TOKENS >>")
-        }
-        return sb.toString()
-    }
-
-    suspend fun chat(prompt: String = "How many paws are in my house?"): String {
-        val chat = textModel.startChat(
-            history = listOf(
-                content(role = "user") { text("Hello, I have 2 dogs in my house.") },
-                content(role = "model") { text("Great to meet you. What would you like to know?") }
-            )
-        )
-        val inputContent = content(role = "user") { text(prompt) }
-
-        val sb = StringBuilder()
-        try {
-            chat.sendMessageStream(inputContent).collect { chunk ->
-                Timber.d(chunk.text)
-                sb.append(chunk.text)
-            }
-        } catch (e: ResponseStoppedException) {
-            sb.append(" << MAX_TOKENS >>")
+            sb.append(MAX_TOKENS)
         }
         return sb.toString()
     }
